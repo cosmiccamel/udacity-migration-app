7b2fe39532db9053ddd68811e8d0554c2705d891
Juan R <5505878+cosmiccamel@users.noreply.github.com>
2021-01-19 04:31:34 +0200

    Update __init__.py

--- function/ServiceBusFunction/__init__.py
+++ function/ServiceBusFunction/__init__.py
@@ -8,49 +8,36 @@ from sendgrid.helpers.mail import Mail
 
 def main(msg: func.ServiceBusMessage):
 
-    try:
-        notification_id = int(msg.get_body().decode('utf-8'))
-        logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
-    except Exception as err:
-        logging.error("kaput")
-        logging.error(err)
+    notification_id = int(msg.get_body().decode('utf-8'))
+    logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
+
+    # Get connection to database
+    connection = psycopg2.connect(  host="techconfdb-server.postgres.database.azure.com",
+                                    dbname="techconfdb",
+                                    user="azureuser@techconfdb-server",
+                                    password="azurepass123$" )
+    cursor = connection.cursor()
     
     try:
-        connection = psycopg2.connect(  host="techconfdb-server.postgres.database.azure.com",
-                                        dbname="techconfdb",
-                                        user="azureuser@techconfdb-server",
-                                        password="azurepass123$" )
-        cursor = connection.cursor()    
-    except Exception as err:
-        logging.error("kaput")
-        logging.error(err)
-
-    try:
-        
-        # Get notification message and subject from database using the notifiation_id
-        logging.info('Get message and subject')
-        cursor.execute("Select message, subject from notification where id= {} ;".format(notification_id))
-        note_query = cursor.fetchone()
+        # Get notification message and subject from database using the notification_id
+        logging.info('Fetching notification message and subject...')
+        notification_query = cursor.execute("SELECT message, subject FROM notification WHERE id = {};".format(notification_id))
 
         # Get attendees email and name
-        logging.info('Get name and email')
+        logging.info('Fetching attendees email and name...')
         cursor.execute("SELECT first_name, last_name, email FROM attendee;")
-        conf_alien = cursor.fetchall()
+        attendees = cursor.fetchall()
 
         # Loop through each attendee and send an email with a personalized subject
-        logging.info('Sending email to attendee ')
-        for human in conf_alien:
-            Mail('{}, {}, {}'.format({'azureuser@techconfdb-server'}, {human[2]}, {note_query}))
+        logging.info('Sending emails...')
+        for attendee in attendees:
+            Mail('{}, {}, {}'.format({'john@doe.com'}, {attendee[2]}, {notification_query}))
 
         # Update the notification table by setting the completed date and updating the status with the total number of attendees notified
-        end_date = datetime.utcnow()
-        logging.info('Update table')
-
-        
-        msg_status = 'Notified {} attendees'.format(len(conf_alien))
-        
-        text_msg = "UPDATE notification SET status = '{}', completed_date = '{}' WHERE id = {};".format(msg_status, end_date, notification_id)
-        update_query = cursor.execute(text_msg)
+        logging.info('Updating notifications...')
+        notification_completed_date = datetime.utcnow()
+        notification_status = 'Notified {} attendees'.format(len(attendees))
+        update_query = cursor.execute("UPDATE notification SET status = '{}', completed_date = '{}' WHERE id = {};".format(notification_status, notification_completed_date, notification_id))
         connection.commit()
 
     except (Exception, psycopg2.DatabaseError) as error:
